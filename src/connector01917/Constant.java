68c94069463f9e722c8b1b822442a1491fd1d239
Anders W. Olsen <anders@wiberg.tech>
2017-05-05 16:44:47 +0200

    fixing db-name for travis

--- src/connector01917/Constant.java
+++ src/connector01917/Constant.java
@@ -7,9 +7,9 @@ public abstract class Constant
 {
 	public static final String
 		server					= "localhost",  // database-serveren
-		database				=  "test-db",  //"jdbcdatabase", // navnet paa din database = dit studienummer
-		username				= "sXXXXXX", // dit brugernavn = dit studienummer 
-		password				= ""; // dit password som du har valgt til din database
+		database				=  "testdb",  //"jdbcdatabase", // navnet paa din database = dit studienummer
+		username				= "root", // dit brugernavn = dit studienummer
+		password				= "travis"; // dit password som du har valgt til din database
 	
 	public static final int
 		port					= 3306;
