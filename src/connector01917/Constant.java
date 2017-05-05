37331194eafdc97a0054db2e165b41b247abf6ac
Anders W. Olsen <anders@wiberg.tech>
2017-05-05 16:39:58 +0200

    Implementing travis testing...

--- src/connector01917/Constant.java
+++ src/connector01917/Constant.java
@@ -6,8 +6,8 @@ package connector01917;
 public abstract class Constant
 {
 	public static final String
-		server					= "sql-lab1.cc.dtu.dk",  // database-serveren
-		database				=  "sXXXXXX",  //"jdbcdatabase", // navnet paa din database = dit studienummer
+		server					= "localhost",  // database-serveren
+		database				=  "test-db",  //"jdbcdatabase", // navnet paa din database = dit studienummer
 		username				= "sXXXXXX", // dit brugernavn = dit studienummer 
 		password				= ""; // dit password som du har valgt til din database
 	
---
+++ test/daoimpl01917/MySQLOperatorDAOTest.java
@@ -0,0 +1,36 @@
+package daoimpl01917;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by AndersWOlsen on 05-05-2017.
+ */
+public class MySQLOperatorDAOTest {
+    @Test
+    public void getOperator() throws Exception {
+        assertTrue(true);
+    }
+
+    @Test
+    public void createOperator() throws Exception {
+        assertTrue(true);
+    }
+
+    @Test
+    public void updateOperator() throws Exception {
+        assertTrue(true);
+    }
+
+    @Test
+    public void deleteOperator() throws Exception {
+        assertTrue(true);
+    }
+
+    @Test
+    public void getOperatorList() throws Exception {
+        assertTrue(!false);
+    }
+
+}
