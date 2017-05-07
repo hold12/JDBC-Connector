6f3aa560b2c53eb0fb236c381beef9986f90f9e1
Troels Just Christoffersen <troelsjc@gmail.com>
2017-05-07 19:52:35 +0200

    created test for MYSQLRecipeDAO

---
+++ test/daoimpl01917/MYSQLRecipeDAOTest.java
@@ -0,0 +1,60 @@
+package daoimpl01917;
+
+import connector01917.IConnector;
+import connector01917.TestConnector;
+import dto01917.RecipeDTO;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by troels on 2017-05-07.
+ */
+public class MYSQLRecipeDAOTest {
+    private final RecipeDTO testRecipe = new RecipeDTO(1, "pizza");
+    private IConnector connector;
+    private MYSQLRecipeDAO recipeDAO;
+
+    @Before
+    public void setUp() throws Exception {
+        connector = new TestConnector();
+        recipeDAO = new MYSQLRecipeDAO(connector);
+    }
+
+    @Test
+    public void getRecipe() throws Exception {
+        RecipeDTO inExpected = testRecipe;
+        RecipeDTO inActual = recipeDAO.getRecipe(1);
+        assertTrue(((TestConnector) connector).isSelected());
+        assertTrue(inExpected.equals(inActual));
+    }
+
+    @Test
+    public void getRecipeList() throws Exception {
+        RecipeDTO inExpected = testRecipe;
+        RecipeDTO inActual = recipeDAO.getRecipeList();
+        assertTrue(((TestConnector) connector).isSelected());
+        assertTrue(inExpected.equals(inActual));
+    }
+
+    @Test
+    public void createRecipe() throws Exception {
+        RecipeDTO inExpected = testRecipe;
+        RecipeDTO inActual = recipeDAO.createRecipe(testRecipe);
+        assertTrue(((TestConnector) connector).isInserted());
+    }
+
+    @Test
+    public void updateRecipe() throws Exception {
+        RecipeDTO newRecipe = new RecipeDTO(2, "Melt cheese");
+        recipeDAO.updateRecipe(newRecipe);
+        assertTrue(((TestConnector) connector).isUpdated());
+    }
+
+    @Test
+    public void deleteRecipe() throws Exception {
+        recipeDAO.deleteRecipe(testRecipe);
+        assertTrue(((TestConnector) connector).isDeleted());
+    }
+}
